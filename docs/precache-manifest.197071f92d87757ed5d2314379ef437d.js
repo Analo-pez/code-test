6cb11751c858cdeb2d7341e747e825d511ee5b34
Analo-pez <ana.ts.lopez@gmail.com>
2020-11-03 19:47:06 +0100

    create docs folder

---
+++ docs/precache-manifest.197071f92d87757ed5d2314379ef437d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "819b6899eddb0a93d979855cc98c2d44",
+    "url": "./index.html"
+  },
+  {
+    "revision": "226d253d67e7fece6de7",
+    "url": "./static/css/main.710a6928.chunk.css"
+  },
+  {
+    "revision": "f3581e9b1c1df2a46652",
+    "url": "./static/js/2.50f8ad53.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.50f8ad53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "226d253d67e7fece6de7",
+    "url": "./static/js/main.1a7a0229.chunk.js"
+  },
+  {
+    "revision": "e209bbc62f631d29b29a",
+    "url": "./static/js/runtime-main.2d1a9480.js"
+  },
+  {
+    "revision": "e5c7d114099c9851a44ffc003b0a331a",
+    "url": "./static/media/backMarket.e5c7d114.png"
+  },
+  {
+    "revision": "e6da1b035161a3a8b40b5964a5d8a0bf",
+    "url": "./static/media/ic_checked.e6da1b03.svg"
+  },
+  {
+    "revision": "7d85da8364ac19b60c72cc11a4407dc0",
+    "url": "./static/media/ic_hide.7d85da83.svg"
+  },
+  {
+    "revision": "c96906cf0bf538f89a5b697df0338d09",
+    "url": "./static/media/loading.c96906cf.jpg"
+  },
+  {
+    "revision": "3ea849d28de5f2240ede0a64403b692b",
+    "url": "./static/media/mercadona.3ea849d2.jpg"
+  },
+  {
+    "revision": "ca76e9eaa2e35599be9fb665c0d9973a",
+    "url": "./static/media/user.ca76e9ea.jpg"
+  }
+]);
